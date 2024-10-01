ffcaf5f0f9e904e7da1afdb0765ac255fc8e59f4
MR-piip-pro <kilhantar@proton.me>
2024-10-01 18:05:18 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,11 @@
-pyautogui
-time
+#!/bin/bash
+
+# تثبيت الحزم المطلوبة باستخدام APT
+echo "Installing required packages..."
+sudo apt update
+sudo apt install -y python3-pip xterm python3-tk
+
+echo "Installing required Python libraries..."
+pip3 install pyautogui rich pyperclip
+
+echo "Completion of the download process. "
