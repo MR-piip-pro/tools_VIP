e1c02e1366bfb1724ff1bee782a393a88886a8f8
MR-piip-pro <kilhantar@proton.me>
2025-01-22 21:51:10 +0100

    Add command_list_1 function with slow colored text printing

---
+++ command_layn/command_6_.py
@@ -0,0 +1,24 @@
+
+import time
+from taym.clear_os import clear_os
+from rich.console import Console
+def command_list_1():
+    # The text that will be printed
+    text = """
+
+    """
+    # Color and time variables
+    color = "bright_blue"  # The selected color of the text
+    delay = 0.001  # The exact time between printing each letter
+    def slow_print_colored(text, color, delay=0.01):
+        # Create a Console object for color printing
+        console = Console()
+        # Loop to repeat each letter in the text
+        for char in text:
+            # Print the character with the selected color without moving to a new line
+            console.print(char, style=color, end='')  
+            # Delay printing the character by the specified amount of time
+            time.sleep(delay)
+        print()  # Move to a new line after printing the entire text
+    # Call the function with text passing, color, delay time
+    slow_print_colored(text, color, delay)
