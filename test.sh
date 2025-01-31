642007c2850bda9fd2f2c1578448a3ba3f2ae775
MR-piip-pro <kilhantar@proton.me>
2025-01-31 18:32:48 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,9 +3,19 @@
 # تثبيت الحزم المطلوبة باستخدام APT
 echo "Installing required packages..."
 sudo apt update
-sudo apt install -y python3-pip xterm python3-tk
+sudo apt install -y python3-pip xterm python3-tk python3-venv
 
+# إنشاء بيئة افتراضية
+echo "Creating virtual environment..."
+python3 -m venv myenv
+
+# تفعيل البيئة الافتراضية
+echo "Activating virtual environment..."
+source myenv/bin/activate
+
+# تثبيت المكتبات المطلوبة في البيئة الافتراضية
 echo "Installing required Python libraries..."
-pip3 install pyautogui rich pyperclip
+pip install pyautogui rich pyperclip
 
-echo "Completion of the download process. "
+echo "Completion of the download process."
+
